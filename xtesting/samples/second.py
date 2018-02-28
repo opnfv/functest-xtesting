6399cfa5f725ace473e40f4093cea62e9a829a70
Cédric Ollivier <cedric.ollivier@orange.com>
2018-02-28 11:51:16 +0100

    Publish Dockerfile and samples
    Change-Id: Ie776fea631f04bb42d8ae80bc658318d8644d0e5
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

---
+++ xtesting/samples/second.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2016 Orange and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# pylint: disable=missing-docstring
+
+from xtesting.core import feature
+
+
+class Test(feature.Feature):
+
+    def execute(self, **kwargs):
+        print "Hello World"
+        return 0
