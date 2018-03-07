d85d9e1a4faba2c4b67fbe92882b81b22943c51b
Delia Popescu <delia.popescu@enea.com>
2018-03-07 12:04:18 +0200

    Add build.sh for functest-xtesting
    JIRA: FUNCTEST-948
    
    Change-Id: If174ac0cc5716ccd6f5bcd6b5627feb222d05b0b
    Signed-off-by: Delia Popescu <delia.popescu@enea.com>

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# SPDX-license-identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2018
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+set -xe
+
+repo=${REPO:-opnfv}
+tag=${BRANCH:-latest}
+arch=${arch_tag-"\
+amd64 \
+arm64"}
+image="functest-xtesting"
+build_opts=(--pull=true --no-cache --force-rm=true)
+
+for arch in ${arch};do
+    if [[ ${arch} == +(aarch64|arm64) ]]; then
+        find . -name Dockerfile -exec sed -i -e "s|alpine:3.7|multiarch/alpine:arm64-v3.7|g" {} +
+    fi
+    (cd docker &&   docker build "${build_opts[@]}" -t "${repo}/${image}:${arch}-${tag}" .)
+    docker push "${repo}/${image}:${arch}-${tag}"
+    [ "$?" == "0" ] && (sudo docker rmi "${repo}/${image}:${arch}-${tag}"|| true)
+    find . -name Dockerfile -exec git checkout \{\} +;
+done
+exit $?
