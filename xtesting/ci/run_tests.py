000f21ee1c0dcba6a438e39166bad8b3270e3c5e
Cédric Ollivier <cedric.ollivier@orange.com>
2018-07-15 11:22:05 +0200

    Fix results when only one skipped test
    Change-Id: Id6c8029b8be1e3a01979feb45daebad08710fe20
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

--- xtesting/ci/run_tests.py
+++ xtesting/ci/run_tests.py
@@ -150,6 +150,7 @@ class Runner(object):
         run_dict = self.get_run_dict(test.get_name())
         if run_dict:
             try:
+                LOGGER.info("Loading test case '%s'...", test.get_name())
                 module = importlib.import_module(run_dict['module'])
                 cls = getattr(module, run_dict['class'])
                 test_dict = Runner.get_dict_by_test(test.get_name())
@@ -158,6 +159,7 @@ class Runner(object):
                 test_case.check_requirements()
                 if test_case.is_skipped:
                     LOGGER.info("Skipping test case '%s'...", test.get_name())
+                    LOGGER.info("Test result:\n\n%s\n", test_case)
                     return testcase.TestCase.EX_TESTCASE_SKIPPED
                 LOGGER.info("Running test case '%s'...", test.get_name())
                 try:
@@ -233,7 +235,7 @@ class Runner(object):
                 elif self.tiers.get_test(kwargs['test']):
                     result = self.run_test(
                         self.tiers.get_test(kwargs['test']))
-                    if result != testcase.TestCase.EX_OK:
+                    if result == testcase.TestCase.EX_TESTCASE_FAILED:
                         LOGGER.error("The test case '%s' failed.",
                                      kwargs['test'])
                         self.overall_result = Result.EX_ERROR
