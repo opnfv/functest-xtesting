5487cab0ab745285512cb87969dd174d86ec9568
Vincent Mahe <v.mahe@orange.com>
2020-05-25 18:32:58 +0200

    Add MTS driver
    It creates a new container xtesting-mts to avoid increase core
    container size.
    
    Signed-off-by: Vincent Mahe <v.mahe@orange.com>
    Change-Id: I59544023e1235747e140a442815778a133bf6acf
    (cherry picked from commit 32eb7687bf7b8440aa87805480789ef61ae65ec9)

--- build.sh
+++ build.sh
@@ -13,22 +13,46 @@ arch=${arch-"\
 amd64 \
 arm64 \
 arm"}
+amd64_dirs=${amd64_dirs-"\
+docker/core \
+docker/mts"}
+arm_dirs=${arm_dirs-${amd64_dirs}}
+arm64_dirs=${arm64_dirs-${amd64_dirs}}
 image="xtesting"
 build_opts=(--pull=true --no-cache --force-rm=true)
 
-for arch in ${arch};do
+for arch in ${arch}; do
     if [[ ${arch} == arm64 ]]; then
         find . -name Dockerfile -exec sed -i \
             -e "s|alpine:3.9|arm64v8/alpine:3.9|g" {} +
+        find . -name Dockerfile -exec sed -i \
+            -e "s|opnfv/xtesting:${tag}|${repo}/xtesting:arm64-${tag}|g" {} +
     elif [[ ${arch} == arm ]]; then
         find . -name Dockerfile -exec sed -i \
             -e "s|alpine:3.9|arm32v6/alpine:3.9|g" {} +
+        find . -name Dockerfile -exec sed -i \
+            -e "s|opnfv/xtesting:${tag}|${repo}/xtesting:arm-${tag}|g" {} +
+    else
+        find . -name Dockerfile -exec sed -i \
+            -e "s|opnfv/xtesting:${tag}|${repo}/xtesting:amd64-${tag}|g" {} +
     fi
-    (cd docker &&   docker build "${build_opts[@]}" \
-        -t "${repo}/${image}:${arch}-${tag}" .)
-    docker push "${repo}/${image}:${arch}-${tag}"
+    dirs=${arch}_dirs
+    for dir in ${!dirs}; do
+        if [[ ${dir} == docker/core ]]; then
+            image=xtesting
+        else
+            image=xtesting-${dir##**/}
+        fi
+        (cd "${dir}" &&
+            docker build "${build_opts[@]}" \
+                -t "${repo}/${image}:${arch}-${tag}" . &&
+            docker push "${repo}/${image}:${arch}-${tag}")
+        [ "${dir}" != "docker/core" ] &&
+            (docker rmi \
+                "${repo}/${image}:${arch}-${tag}" || true)
+    done
     [ "$?" == "0" ] &&
-        (sudo docker rmi "${repo}/${image}:${arch}-${tag}"|| true)
+        (sudo docker rmi "${repo}/xtesting:${arch}-${tag}"|| true)
     find . -name Dockerfile -exec git checkout \{\} +;
 done
 exit $?
