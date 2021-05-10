eddde23de6b133486c6d23a19f9197025a00c459
Vincent Danno <vincent.danno@orange.com>
2021-05-10 11:27:15 +0200

    Use constants instead of hard-coding paths
    Signed-off-by: Vincent Danno <vincent.danno@orange.com>
    Change-Id: I9bef7db7584d6b615ef102d13797914095512733

--- xtesting/utils/constants.py
+++ xtesting/utils/constants.py
@@ -2,4 +2,13 @@
 
 # pylint: disable=missing-docstring
 
+import os
+
 ENV_FILE = '/var/lib/xtesting/conf/env_file'
+
+RESULTS_DIR = '/var/lib/xtesting/results/'
+LOG_PATH = os.path.join(RESULTS_DIR, 'xtesting.log')
+DEBUG_LOG_PATH = os.path.join(RESULTS_DIR, 'xtesting.debug.log')
+
+INI_PATH = 'ci/logging.ini'
+DEBUG_INI_PATH = 'ci/logging.debug.ini'
