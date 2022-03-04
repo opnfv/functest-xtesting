f25fd7cb1b18c9f6346b6dfaa36b47e16e315ddc
Cédric Ollivier <cedric.ollivier@orange.com>
2022-03-04 10:01:29 +0100

    Protect vs Bucket containing html chars
    It allows dumping MTS output files.
    
    Change-Id: I2b35ee81a764573a9d17cb3a225cb5febed5fa46
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

--- xtesting/core/campaign.py
+++ xtesting/core/campaign.py
@@ -15,6 +15,7 @@ import logging.config
 import mimetypes
 import os
 import re
+import urllib
 import zipfile
 
 import boto3
@@ -120,31 +121,29 @@ class Campaign():
             dst_s3_url = os.environ["S3_DST_URL"]
             multipart_threshold = 5 * 1024 ** 5 if "google" in os.environ[
                 "S3_ENDPOINT_URL"] else 8 * 1024 * 1024
-<<<<<<< HEAD
-            config = TransferConfig(multipart_threshold=multipart_threshold)
-            bucket_name = urllib.parse.urlparse(dst_s3_url).netloc
-=======
             tconfig = TransferConfig(multipart_threshold=multipart_threshold)
-            bucket_name = urlparse(dst_s3_url).netloc
->>>>>>> 5ab58cf4 (Search config files in tree)
+            bucket_name = urllib.parse.urlparse(dst_s3_url).netloc
             s3path = re.search(
                 '^/*(.*)/*$', urllib.parse.urlparse(dst_s3_url).path).group(1)
             prefix = os.path.join(s3path, build_tag)
             # pylint: disable=no-member
             for s3_object in b3resource.Bucket(bucket_name).objects.filter(
-                    Prefix="{}/".format(prefix)):
-                path, _ = os.path.split(s3_object.key)
-                lpath = re.sub('^{}/*'.format(s3path), '', path)
+                    Prefix=f"{prefix}/"):
+                path, _ = os.path.split(
+                    urllib.parse.unquote_plus(s3_object.key))
+                lpath = re.sub(f'^{s3path}/*', '', path)
                 if lpath and not os.path.exists(lpath):
                     os.makedirs(lpath)
-                # pylint: disable=no-member
-                b3resource.Bucket(bucket_name).download_file(
-                    s3_object.key,
-                    re.sub(f'^{s3path}/*', '', s3_object.key),
-                    Config=tconfig)
                 Campaign.__logger.info(
                     "Downloading %s",
-                    re.sub('^{}/*'.format(s3path), '', s3_object.key))
+                    re.sub(f'^{s3path}/*', '',
+                           urllib.parse.unquote_plus(s3_object.key)))
+                # pylint: disable=no-member
+                b3resource.Bucket(bucket_name).download_file(
+                    urllib.parse.unquote_plus(s3_object.key),
+                    re.sub(f'^{s3path}/*', '',
+                           urllib.parse.unquote_plus(s3_object.key)),
+                    Config=tconfig)
             return Campaign.EX_OK
         except Exception:  # pylint: disable=broad-except
             Campaign.__logger.exception("Cannot publish the artifacts")
@@ -189,9 +188,9 @@ class Campaign():
             multipart_threshold = 5 * 1024 ** 5 if "google" in os.environ[
                 "S3_ENDPOINT_URL"] else 8 * 1024 * 1024
             tconfig = TransferConfig(multipart_threshold=multipart_threshold)
-            bucket_name = urlparse(dst_s3_url).netloc
+            bucket_name = urllib.parse.urlparse(dst_s3_url).netloc
             mime_type = mimetypes.guess_type(f'{build_tag}.zip')
-            path = urlparse(dst_s3_url).path.strip("/")
+            path = urllib.parse.urlparse(dst_s3_url).path.strip("/")
             # pylint: disable=no-member
             b3resource.Bucket(bucket_name).upload_file(
                 f'{build_tag}.zip',
