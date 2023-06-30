a7502fc7e439b7215e2a507813418c929e176187
guillaume.lambert <guillaume.lambert@orange.com>
2023-06-30 10:57:28 +0200

    Fix README.md Dockerfile sample
    Current Dockerfile sample does not work anymore.
    - bump alpine version to 3.18
    - add libxml2 and libxslt dev packages installation
    - add gcc package installation
    
    Change-Id: I5315dc5277674dcd32b07344534681d29ef9d5e6
    Signed-off-by: guillaume.lambert <guillaume.lambert@orange.com>

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Apache License, Version 2.0
@@ -19,15 +19,29 @@ arm_dirs=${arm_dirs-${amd64_dirs}}
 arm64_dirs=${arm64_dirs-${amd64_dirs}}
 tag=${BRANCH:-latest}
 image="xtesting"
-build_opts=(--pull=true --no-cache --force-rm=true)
+build_opts="--pull=true --no-cache --force-rm=true"
+
+if [ `which podman` ];then
+    DOCKER_CMD=podman
+else
+    if [ ! `which $DOCKER_CMD` ];then
+        echo "no docker command available" >&2
+        exit 1
+    fi
+    #if "docker ps" cannot be run without error, prepend sudo
+    if ( ! $DOCKER_CMD ps >/dev/null 2>&1 );then
+        echo "docker command only usable as root, using sudo" >&2
+        DOCKER_CMD="sudo docker"
+    fi
+fi
 
 for arch in ${arch}; do
-    if [[ ${arch} == arm64 ]]; then
+    if [ "${arch}" = "arm64" ]; then
         find . -name Dockerfile -exec sed -i \
             -e "s|alpine:3.15|arm64v8/alpine:3.15|g" {} +
         find . -name Dockerfile -exec sed -i \
             -e "s|opnfv/xtesting|${repo}/xtesting:arm64-${tag}|g" {} +
-    elif [[ ${arch} == arm ]]; then
+    elif [ "${arch}" = "arm" ]; then
         find . -name Dockerfile -exec sed -i \
             -e "s|alpine:3.15|arm32v6/alpine:3.15|g" {} +
         find . -name Dockerfile -exec sed -i \
@@ -37,22 +51,24 @@ for arch in ${arch}; do
             -e "s|opnfv/xtesting|${repo}/xtesting:amd64-${tag}|g" {} +
     fi
     dirs=${arch}_dirs
-    for dir in ${!dirs}; do
-        if [[ ${dir} == docker/core ]]; then
+    # eval presents no security issue here if possible values of ${arch} are constrained
+    eval dirlist=\$$dirs
+    for dir in $dirlist; do
+        if [ "${dir}" = "docker/core" ]; then
             image=xtesting
         else
             image=xtesting-${dir##**/}
         fi
         (cd "${dir}" &&
-            docker build "${build_opts[@]}" \
+            $DOCKER_CMD build $build_opts \
                 -t "${repo}/${image}:${arch}-${tag}" . &&
-            docker push "${repo}/${image}:${arch}-${tag}")
+            $DOCKER_CMD push "${repo}/${image}:${arch}-${tag}")
         [ "${dir}" != "docker/core" ] &&
-            (docker rmi \
+            ($DOCKER_CMD rmi \
                 "${repo}/${image}:${arch}-${tag}" || true)
     done
-    [ "$?" == "0" ] &&
-        (sudo docker rmi "${repo}/xtesting:${arch}-${tag}"|| true)
+    [ "$?" = "0" ] &&
+        ($DOCKER_CMD rmi "${repo}/xtesting:${arch}-${tag}"|| true)
     find . -name Dockerfile -exec git checkout \{\} +;
 done
 exit $?
